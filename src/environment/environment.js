7f976a23bc53434ca229c3bf285c741e47cf680d
Jair Velasquez <jairvelazpizar@gmail.com>
2025-06-18 18:03:59 -0500

    feat: changes on env

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
-    // baseUrl: "https://fromzeroapi.onrender.com/api/v1",
+    baseUrl: "https://fromzeroapi.onrender.com/api/v1",
     // supabaseUrl: 'https://hwqkibwyspmfwkzjlumy.supabase.co',
     // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3cWtpYnd5c3BtZndrempsdW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1NDA0NDIsImV4cCI6MjA0NzExNjQ0Mn0.By3x97OAlfZ26rRIwFC2GDblhpc6p7fTxUY-83QO4cY'
 
-    baseUrl: "http://localhost:8080/api/v1/",
+    //baseUrl: "http://localhost:8080/api/v1/",
     supabaseUrl: 'https://hwqkibwyspmfwkzjlumy.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3cWtpYnd5c3BtZndrempsdW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1NDA0NDIsImV4cCI6MjA0NzExNjQ0Mn0.By3x97OAlfZ26rRIwFC2GDblhpc6p7fTxUY-83QO4cY'
 
